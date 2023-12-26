c77f4020f8b20cbfdcbaa2c2df88d94cdd003822
Arshia <arshia@CCS-G40MPN3>
2023-12-26 14:28:21 -0500

    added search and local storage

--- api/management/commands/generate_random_data.py
+++ api/management/commands/generate_random_data.py
@@ -13,19 +13,38 @@ class Command(BaseCommand):
         Test.objects.all().delete()
 
 
-        # List of possible test types
-        types = ['Type1', 'Type2', 'Type3', 'Type4', 'Type5']
+        # Names
+        names = ['Tester1', ' Tester2', ' Tester3', ' Tester4', ' Tester5']
 
+        programs = ['Program1', ' Program2', ' Program3', ' Program4', ' Program5']
+
+
+        # List of possible test types
+        scripts = ['Script1', 'Script2', 'Script3', 'Script4', 'Script5']
+
+
+       
         # List of possible progress states
-        progress_states = ['Not Started', 'In Progress', 'Completed']
+        progress_states = [Test.NOT_STARTED, Test.IN_PROGRESS, Test.COMPLETED, Test.FAILED]
+
+        # List of possible engineers and technicians
+        engineers = ['Engineer1', 'Engineer2', 'Engineer3']
+        technicians = ['Technician1', 'Technician2', 'Technician3']
+
 
         # Generating a list of Test objects
         test_objects = [
             {
-                'type': random.choice(types),
+                'name': random.choice(names),
+                'program': random.choice(programs),
+                'script': random.choice(scripts),
+                'code_line': random.randint(0, 100),
+                'engineer': random.choice(engineers),
+                'technician': random.choice(technicians),
                 'progress': random.choice(progress_states),
-                'date_started': timezone.now() - timedelta(days=random.randint(0, 30)),  # Random date in the last 30 days
-                'date_ended': timezone.now() if random.random() < 0.5 else None,  # 50% chance of being None
+                'start_date': timezone.now() - timedelta(days=random.randint(0, 30)),  # Random date in the last 30 days
+                'end_date': timezone.now() if random.random() < 0.5 else None,  # 50% chance of being None
+                'fail_date': None
             }
             for _ in range(100)  # Change this number to the number of objects you want to create
         ]
@@ -33,4 +52,6 @@ class Command(BaseCommand):
         # Now, you can import this data in the Django shell
 
         for obj in test_objects:
+            if obj['progress'] == Test.FAILED:
+                obj['fail_date'] = timezone.now() - timedelta(days=random.randint(0, 30))
             Test.objects.create(**obj)
--- frontend/src/components/TestList.js
+++ frontend/src/components/TestList.js
@@ -8,19 +8,187 @@ import {
   Menu,
   MenuItem,
   Select,
+  TextField,
+  InputAdornment,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 export default function TestList() {
   const [rows, setRows] = useState([]);
-  const [view, setView] = useState("manager");
 
-  const columns = [
+  const [search, setSearch] = useState("");
+
+  const [view, setView] = useState(
+    JSON.parse(localStorage.getItem("view")) || "manager"
+  );
+  const [sortModel, setSortModel] = useState(
+    JSON.parse(localStorage.getItem("sortModel")) || []
+  );
+  const [filterModel, setFilterModel] = useState(
+    JSON.parse(localStorage.getItem("filterModel")) || { items: [] }
+  );
+  const [page, setPage] = useState(
+    JSON.parse(localStorage.getItem("page")) || 0
+  );
+  const [columnVisibilityModel, setColumnVisibilityModel] = useState(
+    JSON.parse(localStorage.getItem("columnVisibilityModel")) || {
+      name: view !== "manager",
+      script: view !== "manager",
+      code_line: view !== "manager",
+      engineer: view !== "manager",
+      technician: view !== "manager",
+    }
+  );
+  const [paginationModel, setPaginationModel] = useState(
+    JSON.parse(localStorage.getItem("paginationModel")) || {
+      pageSize: 15,
+      page: 0,
+    }
+  );
+
+  // Fetching data for datagrid
+  useEffect(() => {
+    fetch("/api/test-list/")
+      .then((response) => response.json())
+      .then((data) => setRows(data))
+      .catch((error) => console.log("Error"));
+  }, []);
+
+  // Saving state to local storage on datagrid property change
+  useEffect(() => {
+    localStorage.setItem("sortModel", JSON.stringify(sortModel));
+    localStorage.setItem("filterModel", JSON.stringify(filterModel));
+    localStorage.setItem("page", JSON.stringify(page));
+    localStorage.setItem(
+      "columnVisibilityModel",
+      JSON.stringify(columnVisibilityModel)
+    );
+    localStorage.setItem("paginationModel", JSON.stringify(paginationModel));
+    localStorage.setItem("view", JSON.stringify(view));
+  }, [
+    sortModel,
+    filterModel,
+    page,
+    columnVisibilityModel,
+    paginationModel,
+    view,
+  ]);
+
+  const handleViewChange = (event) => {
+    setView(event.target.value);
+    // You can use useEffect to do this better with just the view value
+    // setView is async but doesn't return a promise
+    const newModel = {
+      name: event.target.value !== "manager",
+      script: event.target.value !== "manager",
+      code_line: event.target.value !== "manager",
+      engineer: event.target.value !== "manager",
+      technician: event.target.value !== "manager",
+    };
+    setColumnVisibilityModel(newModel);
+    localStorage.setItem("columnVisibilityModel", JSON.stringify(newModel));
+    console.log(view);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  // .filter takes a filtering function as an argument and returns
+  // the filtered array
+  const filteredRows = rows.filter((row) =>
+    // Object.values returns an array of the values of an object
+    // .some returns true if at least one element in the array matches the given filter function passed in
+    Object.values(row).some((value) => {
+      // This final line just checks if the given value includes the search value
+      if (value !== null) {
+        return value.toString().toLowerCase().includes(search.toLowerCase());
+      }
+      return false;
+    })
+  );
+
+  if (rows === null) {
+    <Box
+      style={{ height: "100vh" }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography component="h1" variant="h1">
+        Loading...
+      </Typography>
+    </Box>;
+  }
+  // else return datagrid
+  return (
+    <Container style={{ marginTop: 20, height: "150vh" }}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <TextField
+          value={search}
+          onChange={handleSearchChange}
+          label="Search"
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          style={{ marginBottom: 10 }}
+        />
+        <Select
+          value={view}
+          onChange={handleViewChange}
+          style={{ position: "relative", marginLeft: 14, marginBottom: 10 }}
+        >
+          <MenuItem value={"manager"}>Manager View</MenuItem>
+          <MenuItem value={"eng"}>Engineer View</MenuItem>
+          <MenuItem value={"tech"}>Technician View</MenuItem>
+        </Select>
+      </Box>
+      <Box sx={{ height: "50%", width: "100%" }}>
+        <DataGrid
+          rows={filteredRows}
+          columns={getColumns()}
+          // Setting initial state from local storage
+          sortModel={sortModel}
+          onSortModelChange={(model) => setSortModel(model)}
+          filterModel={filterModel}
+          onFilterModelChange={(model) => setFilterModel(model)}
+          paginationModel={paginationModel}
+          onPaginationModelChange={(model) => setPaginationModel(model)}
+          columnVisibilityModel={columnVisibilityModel}
+          onColumnVisibilityModelChange={(model) => {
+            setColumnVisibilityModel(model);
+          }}
+          //----------------------------------------
+          // initialState={{
+          //   pagination: {
+          //     paginationModel: {
+          //       pageSize: 15,
+          //     },
+          //   },
+          // }}
+          pageSizeOptions={[15]}
+          disableRowSelectionOnClick
+          slots={{
+            toolbar: GridToolbar,
+          }}
+        />
+      </Box>
+    </Container>
+  );
+}
+
+function getColumns() {
+  return [
     {
       field: "name",
       headerName: "Tester Name",
       width: 150,
-      hide: true,
     },
     {
       field: "program",
@@ -36,6 +204,17 @@ export default function TestList() {
       field: "progress",
       headerName: "Progress",
       width: 150,
+      renderCell: (params) => {
+        let color;
+        if (params.value === "In Progress") {
+          color = "#F8E95F";
+        } else if (params.value === "Completed") {
+          color = "#66CAEC";
+        } else if (params.value === "Failed") {
+          color = "#FF6347";
+        }
+        return <span style={{ color: color }}>{params.value}</span>;
+      },
     },
     {
       field: "code_line",
@@ -71,72 +250,4 @@ export default function TestList() {
       width: 150,
     },
   ];
-
-  // Fetching data for datagrid
-
-  useEffect(() => {
-    fetch("/api/test-list/")
-      .then((response) => response.json())
-      .then((data) => setRows(data))
-      .catch((error) => console.log("Error"));
-  }, []);
-
-  const handleChangeView = (event) => {
-    setView(event.target.value);
-    console.log(view);
-  };
-
-  if (rows === null) {
-    <Box
-      style={{ height: "100vh" }}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Typography component="h1" variant="h1">
-        Loading...
-      </Typography>
-    </Box>;
-  }
-  // else return datagrid
-  return (
-    <Container style={{ marginTop: 20, height: "150vh" }}>
-      <Box display="flex" justifyContent="flex-end" alignItems="center">
-        <Select
-          value={view}
-          onChange={handleChangeView}
-          style={{ position: "relative", marginLeft: 14, marginBottom: 10 }}
-        >
-          <MenuItem value={"manager"}>Manager View</MenuItem>
-          <MenuItem value={"eng"}>Engineer View</MenuItem>
-          <MenuItem value={"tech"}>Technician View</MenuItem>
-        </Select>
-      </Box>
-      <Box sx={{ height: "50%", width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 15,
-              },
-            },
-          }}
-          pageSizeOptions={[15]}
-          disableRowSelectionOnClick
-          slots={{
-            toolbar: GridToolbar,
-          }}
-          columnVisibilityModel={{
-            name: view !== "manager",
-            script: view !== "manager",
-            code_line: view !== "manager",
-            engineer: view !== "manager",
-            technician: view !== "manager",
-          }}
-        />
-      </Box>
-    </Container>
-  );
 }
